dbabd24585c3fdac3e6d40fa6984f1e34cb1a999
neverm25 <never08mind25@gmail.com>
2024-11-01 02:52:52 -0400

    chore: remove unused files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-// import Page from './Page';
 import Page from "./LandingPage";
 import { findArbitrageOpportunities } from './arbitrageOpportunities';
 
